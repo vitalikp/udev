ff2a365a9197892b75e5af45a17e845ffd6b6ec2
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-05-29 03:37:59 +0300

    * generate header files before build;

--- src/keyboard-keys.cmake
+++ src/keyboard-keys.cmake
@@ -1,30 +1,32 @@
 # keyboard-keys cmake file
 
-set(KEYS_TXT keyboard-keys.txt)
-set(KEYS_GPERF keyboard-keys-from-name.gperf)
-set(KEYS_FROM_H keyboard-keys-from-name.h)
-set(KEYS_TO_H keyboard-keys-to-name.h)
-add_custom_target(
-	${KEYS_TXT} ALL
-	COMMAND gcc -E -dM -include linux/input.h - < /dev/null | ${AWK} "'/^#define[" \t]+KEY_[^ ]+[ \t]+[0-9]/ { if ($$2 != \"KEY_MAX\") { print $$2 } "}'" > ${KEYS_TXT}
-	#WORKING_DIRECTORY ...
+set(KEYS_TXT src/keyboard-keys.txt)
+set(KEYS_GPERF src/keyboard-keys-from-name.gperf)
+set(KEYS_FROM_H src/keyboard-keys-from-name.h)
+set(KEYS_TO_H src/keyboard-keys-to-name.h)
+execute_process(
+	COMMAND gcc -E -dM -include linux/input.h - 
+	COMMAND ${AWK} "/^#define[ \t]+KEY_[^ ]+[ \t]+[0-9]/ { if ($2 != \"KEY_MAX\") { print $2 } }" 
+	INPUT_FILE /dev/null
+	OUTPUT_FILE ${KEYS_TXT}
 )
-add_custom_target(
-	${KEYS_GPERF} ALL
-	COMMAND ${AWK} 'BEGIN{ print \"struct key { const char* name\; unsigned short id\; }\;\"\; print \"%null-strings\"\; print \"%%\"\;} { print tolower(substr($$1 ,5)) \", \" $$1 }' < ${KEYS_TXT} > ${KEYS_GPERF}
-	DEPENDS ${KEYS_TXT}
-	#WORKING_DIRECTORY ...
+message("-- generate ${KEYS_TXT}")
+execute_process(
+	COMMAND ${AWK} "BEGIN{ print \"struct key { const char* name; unsigned short id; };\"; print \"%null-strings\"; print \"%%\";} { print tolower(substr($$1 ,5)) \", \" $$1 }"
+	INPUT_FILE ${KEYS_TXT}
+	OUTPUT_FILE ${KEYS_GPERF}
 )
-add_custom_target(
-	${KEYS_FROM_H} ALL
-	COMMAND ${GPERF} -L ANSI-C -t -N keyboard_lookup_key -H hash_key_name -p -C < ${KEYS_GPERF} > ${KEYS_FROM_H}
-	DEPENDS ${KEYS_GPERF}
-	#WORKING_DIRECTORY ...
+message("-- generate ${KEYS_GPERF}")
+execute_process(
+	COMMAND ${GPERF} -L ANSI-C -t -N keyboard_lookup_key -H hash_key_name -p -C
+	INPUT_FILE ${KEYS_GPERF}
+	OUTPUT_FILE ${KEYS_FROM_H}
 )
-add_custom_target(
-	${KEYS_TO_H} ALL
-	COMMAND ${AWK} 'BEGIN{ print \"const char* const key_names[KEY_CNT] = { \"} { print \"[\" $$1 \"] = \\\"\" $$1 \"\\\",\" } END{print \"}\;\"}' < ${KEYS_TXT} > ${KEYS_TO_H}
-	DEPENDS ${KEYS_TXT}
-	#WORKING_DIRECTORY ...
+message("-- generate ${KEYS_FROM_H}")
+execute_process(
+	COMMAND ${AWK} "BEGIN{ print \"const char* const key_names[KEY_CNT] = { \"} { print \"[\" $$1 \"] = \\\"\" $$1 \"\\\",\" } END{print \"};\"}"
+	INPUT_FILE ${KEYS_TXT}
+	OUTPUT_FILE ${KEYS_TO_H}
 )
+message("-- generate ${KEYS_TO_H}")
 include_directories(${PROJECT_BINARY_DIR}/src)
