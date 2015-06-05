831b02cde192dd6d88f4fd892a4fe04588f82b5a
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-06-05 20:10:13 +0300

    * man: generate man if source file modified;

--- man/udev-man.cmake
+++ man/udev-man.cmake
@@ -7,14 +7,13 @@ set(XSLTPROC_OPT ${XSLTPROC_OPT} --stringparam funcsynopsis.style ansi)
 set(XSLTPROC_OPT ${XSLTPROC_OPT} --stringparam udev.version ${VERSION})
 set(XSLTPROC_OPT ${XSLTPROC_OPT} --path '${PROJECT_BINARY_DIR}/man:${CMAKE_SOURCE_DIR}/man')
 
-add_custom_target(man ALL)
 
-
-macro(add_man name section)
+macro(add_man var name section)
 	add_custom_command(
-		TARGET man
+		OUTPUT ${name}.${section}
 		COMMAND ${XSLTPROC}
 		ARGS -o ${name}.${section} ${XSLTPROC_OPT} ${CMAKE_SOURCE_DIR}/man/custom-man.xsl ${CMAKE_SOURCE_DIR}/man/${name}.xml
 		COMMENT "  XSLT\t${name}.${section}" VERBATIM)
+		list(APPEND ${var} ${name}.${section})
 	install(FILES ${PROJECT_BINARY_DIR}/${name}.${section} DESTINATION share/man/man${section})
 endmacro(add_man)
