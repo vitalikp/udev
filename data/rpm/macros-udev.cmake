fb52dd19eeb12810ed5272cab3a4a3da5d3c1b90
Perevertun V.M. <vitalik.perevertun@gmail.com>
2015-11-20 23:54:10 +0200

    * data: don't hardcode the binary paths in rpm macros, rely on $PATH;

--- data/rpm/macros-udev.cmake
+++ data/rpm/macros-udev.cmake
@@ -5,9 +5,9 @@
 %_udevhwdbdir @udevhwdbdir@
 
 %udev_hwdb_update() \
-@CMAKE_INSTALL_PREFIX@/bin/udevadm hwdb --update >/dev/null 2>&1 || : \
+udevadm hwdb --update >/dev/null 2>&1 || : \
 %{nil}
 
 %udev_rules_update() \
-@CMAKE_INSTALL_PREFIX@/bin/udevadm control --reload >/dev/null 2>&1 || : \
+udevadm control --reload >/dev/null 2>&1 || : \
 %{nil}
