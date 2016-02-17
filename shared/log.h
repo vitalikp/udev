1a9a83971668d2396234e88b9b2e0f9b4072f23f
Perevertun V.M. <vitalik.perevertun@gmail.com>
2016-02-17 03:08:43 +0200

    * log: remove MESSAGE_ID macros;

--- shared/log.h
+++ shared/log.h
@@ -169,6 +169,4 @@ bool log_on_console(void) _pure_;
 const char *log_target_to_string(LogTarget target) _const_;
 LogTarget log_target_from_string(const char *s) _pure_;
 
-#define MESSAGE_ID(x) "MESSAGE_ID=" SD_ID128_FORMAT_STR, SD_ID128_FORMAT_VAL(x)
-
 void log_received_signal(int level, const struct signalfd_siginfo *si);
