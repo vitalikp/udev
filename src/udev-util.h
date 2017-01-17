383b53e323502e1f620c187e0afd6b5ec7944a9f
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-01-17 15:41:27 +0200

    * remove _cleanup_udev_unref_ auto cleanup macros;

--- src/udev-util.h
+++ src/udev-util.h
@@ -24,7 +24,6 @@
 #include "udev.h"
 #include "util.h"
 
-DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev*, udev_unref);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_device*, udev_device_unref);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_enumerate*, udev_enumerate_unref);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_event*, udev_event_unref);
@@ -32,7 +31,6 @@ DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_rules*, udev_rules_unref);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_ctrl*, udev_ctrl_unref);
 DEFINE_TRIVIAL_CLEANUP_FUNC(struct udev_monitor*, udev_monitor_unref);
 
-#define _cleanup_udev_unref_ _cleanup_(udev_unrefp)
 #define _cleanup_udev_device_unref_ _cleanup_(udev_device_unrefp)
 #define _cleanup_udev_enumerate_unref_ _cleanup_(udev_enumerate_unrefp)
 #define _cleanup_udev_event_unref_ _cleanup_(udev_event_unrefp)
